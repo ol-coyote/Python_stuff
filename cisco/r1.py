0d2d51921152f70e102def107c94ade193df0b0d
root <root@localhost.localdomain>
2020-05-21 17:19:42 -0400

    added cisco telnet from GNS3 class to repo

---
+++ cisco/r1.py
@@ -0,0 +1,31 @@
+#!/bin/bash
+import getpass
+import sys
+import telnetlib
+
+def conn_device(HOST):
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until("Username: ".encode('latin-1'))
+    tn.write("user\n".encode('latin-1'))
+    tn.read_until("Password: ".encode('latin-1'))
+    tn.write("password\n".encode('latin-1'))
+    tn.write("enable\n".encode('latin-1'))
+    tn.write('password\n'.encode('latin-1'))
+    tn.write("conf t\n".encode('latin-1'))
+    tn.write("int loop 0\n".encode('latin-1'))
+    tn.write("ip addr 1.2.3.4 255.255.255.255\n".encode('latin-1'))
+    tn.write('no shut\n'.encode('latin-1'))
+    tn.write("end\n".encode('latin-1'))
+    tn.write("exit\n".encode('latin-1'))
+    print(tn.read_all())
+
+def main():
+    HOST = sys.argv[1]
+    print(f"Connecting to {HOST} via telnet!")
+    conn_device(HOST)
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
